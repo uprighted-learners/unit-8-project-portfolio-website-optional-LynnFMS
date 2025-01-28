8578d1211172d3dbe5911a67f2db4a60c460e744
LynnFMS <lynn1312@gmail.com>
2025-01-27 23:38:17 -0500

    projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -16,11 +16,28 @@ function Projects() {
 
       <h1>Lab Links</h1>
       <ul>
-        <li><a href="https://github.com/uprighted-learners/unit-1-project-jeopardy-LynnFMS" target="_blank" rel="noopener noreferrer">Jeopardy</a></li>
-        <li><a href="https://github.com/uprighted-learners/unit-2-project-guess-the-number-LynnFMS" target="_blank" rel="noopener noreferrer">Guess the Number</a></li>
-        <li><a href="https://github.com/uprighted-learners/unit-3-project-zorkington-LynnFMS" target="_blank" rel="noopener noreferrer">Zorkington</a></li>
-        <li><a href="https://github.com/uprighted-learners/unit-5-project-server-blog-LynnFMS" target="_blank" rel="noopener noreferrer">Server Blog</a></li>
-        <li><a href="https://github.com/uprighted-learners/unit-6-project-react-chat-RolusC5B" target="_blank" rel="noopener noreferrer">React Chat</a></li>
+        <li><a href="https://github.com/LynnFMS/floating-images.git" target="_blank" rel="noopener noreferrer">Floating Images</a></li>
+        <li><a href="https://github.com/LynnFMS/hello-html.git" target="_blank" rel="noopener noreferrer">Hello HTML</a></li>
+        <li><a href="https://github.com/LynnFMS/hello-styles.git" target="_blank" rel="noopener noreferrer">Hello styles</a></li>
+        <li><a href="https://github.com/LynnFMS/image-galary.git" target="_blank" rel="noopener noreferrer">Image Gallary</a></li>
+        <li><a href="https://github.com/LynnFMS/terminal" target="_blank" rel="noopener noreferrer">Terminal</a></li>
+        <li><a href="https://github.com/LynnFMS/capitalize.git" target="_blank" rel="noopener noreferrer">Capitalize</a></li>
+        <li><a href="https://github.com/LynnFMS/combining-data-types.git" target="_blank" rel="noopener noreferrer">Combining Data Types</a></li>
+        <li><a href="https://github.com/LynnFMS/fizzbuzz" target="_blank" rel="noopener noreferrer">Fizz Buzz</a></li>
+        <li><a href="https://github.com/LynnFMS/hello-frenemy.git" target="_blank" rel="noopener noreferrer">Hello Frenemy</a></li>
+        <li><a href="https://github.com/LynnFMS/potato-poem.git" target="_blank" rel="noopener noreferrer">Potato Poem</a></li>
+        <li><a href="https://github.com/LynnFMS/class-gpa.git" target="_blank" rel="noopener noreferrer">Class GPA</a></li>
+        <li><a href="https://github.com/LynnFMS/menu-order.git" target="_blank" rel="noopener noreferrer">Menu Order</a></li>
+        <li><a href="https://github.com/LynnFMS/walkway-objects.git" target="_blank" rel="noopener noreferrer">Walkway Objects</a></li>
+        <li><a href="https://github.com/LynnFMS/rick-and-morty.git" target="_blank" rel="noopener noreferrer">Rick and Morty</a></li>
+        <li><a href="https://github.com/LynnFMS/creating-cookies.git" target="_blank" rel="noopener noreferrer">Creating Cookies</a></li>
+        <li><a href="https://github.com/LynnFMS/hello-files.git" target="_blank" rel="noopener noreferrer">Hello Files</a></li>
+        <li><a href="https://github.com/LynnFMS/poke-blog.git" target="_blank" rel="noopener noreferrer">Poke Blog</a></li>
+        <li><a href="https://github.com/LynnFMS/robot-warehouse.git" target="_blank" rel="noopener noreferrer">Robot Warehouse</a></li>
+        <li><a href="https://github.com/LynnFMS/name-display.git" target="_blank" rel="noopener noreferrer">Name Display</a></li>
+        <li><a href="https://github.com/LynnFMS/post-selector.git" target="_blank" rel="noopener noreferrer">Post Selector</a></li>
+        <li><a href="https://github.com/LynnFMS/react-router.git" target="_blank" rel="noopener noreferrer">React Router</a></li>
+
       </ul>
       </div>
 
